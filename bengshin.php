785b1286c49a06b609d75abe029531b31abb146e
siputra12 <siputra0770@gmail.com>
2021-12-24 21:09:39 +0700

    Create bengshin.php

---
+++ bengshin.php
@@ -0,0 +1,43 @@
+<?php
+echo "Coded By Tra\n";
+$xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx = array("Ref","Nom","Otp");
+echo "?{$xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx[0]}	";
+$xxxxxxxxxx = trim(fgets(STDIN));
+echo "?{$xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx[1]}	";
+$xxxxxxxxxxx = trim(fgets(STDIN));
+for($ggggggggggggggggggg=0;$ggggggggggggggggggg<3;$ggggggggggggggggggg++){
+	$asaloleasuasuhehakwoaksodkhahahawkekwkekaksdasdgdkahfasdajfjsehfgh8123jafahfz = "asaloleasuasuhehakwoaksodkhahahawkekwkekaksdasdgdkahfasdajfjsehfgh8123jafahfg";
+	$xxxxxxxxxxxxxxxxxxxxxxxxxxxx = "xxxxxxxxxxxxxxxxxxxxxxxxxxxx"; $asdafwygtfywef = "Coded ";
+	$xxxxxxxxxxxxs = "file_get_contents";
+	$xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx = $xxxxxxxxxxxxxxxxxxxxxxxxxxxx(5, true)."-".$xxxxxxxxxxxxxxxxxxxxxxxxxxxx(16, true);
+	$xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"; $asdafwygtfywef .= "By ";
+	$asaloleasuasuhehakwoaksodkhahahawkekwkekaksdasdgdkahfasdajfjsehfgh8123jafahf = "asaloleasuasuhehakwoaksodkhahahawkekwkekaksdasdgdkahfasdajfjsehfgh8123jafahfz"; $asdafwygtfywef .= "Tra";
+	$xxxxxxxxxxxxz = "stripos";
+	$xxxxxxxxx = $$asaloleasuasuhehakwoaksodkhahahawkekwkekaksdasdgdkahfasdajfjsehfgh8123jafahf("sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss", $xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx, $xxxxxxxxxxx);
+	$xxxxxxxxxxxxxxxx = $$asaloleasuasuhehakwoaksodkhahahawkekwkekaksdasdgdkahfasdajfjsehfgh8123jafahf("sucksssssssssssssssssssx", $xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx, $xxxxxxxxxxx, $xxxxxxxxx);
+	if(!$xxxxxxxxxxxxz($xxxxxxxxxxxxs("shell.php"), $asdafwygtfywef) OR !$xxxxxxxxxxxxz($xxxxxxxxxxxxs("shell.php"), 'if(!$xxxxxxxxxxxxz($xxxxxxxxxxxxs("shell.php"), $asdafwygtfywef) OR !$xxxxxxxxxxxxz($xxxxxxxxxxxxs("shell.php")')) exit("Gatau Pokoknya Error!!");
+	print_r("$xxxxxxxxxxxxxxxx\n");
+	echo "?{$xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx[2]}	";
+	$xxxxx = trim(fgets(STDIN));
+	$xxxxxx = $$asaloleasuasuhehakwoaksodkhahahawkekwkekaksdasdgdkahfasdajfjsehfgh8123jafahf("suckssssssssssssssssssssssssssssssssssssssssssssssssss", $xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx, $xxxxxxxxxxx, $xxxxxxxxx, $xxxxx);
+	$xxxxxxxxxxxxxxx = $xxxxxxxxxxxxxxxxxxxxxxxxxxxx(4,true);
+	$xxxxxxxxxxxxxxxxxxxxx = $xxxxxxxxxxxxxxxxxxxxxxxxxxxx(5, true);
+	$xxxxxxxxxxxxxxxxxxxxxxxxx = "$xxxxxxxxxxxxxxx$xxxxxxxxxxxxxxxxxxxxx".rand(123,999)."@gmail.com";
+	$xIx = $$asaloleasuasuhehakwoaksodkhahahawkekwkekaksdasdgdkahfasdajfjsehfgh8123jafahf("$xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", $xxxxxxxxxx, $xxxxxxxxxxxxxxx, $xxxxxxxxxxxxxxxxxxxxx, $xxxxxxxxxxxxxxxxxxxxxxxxx, $xxxxxxxxxxx,  $xxxxxx, $xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx);
+	echo "$xIx\n";
+	@file_put_contents("akun_tuyul_shell_asia.txt", "$xxxxxxxxxxxxxxxxxxxxxxxxx | $xxxxxx | $xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx | $xxxxxxxxxxx\n", FILE_APPEND);
+}
+
+function asaloleasuasuhehakwoaksodkhahahawkekwkekaksdasdgdkahfasdajfjsehfgh8123jafahfg($cmd, $a = null, $b = null, $c = null, $d = null, $e = null, $f = null){
+	return @file_get_contents("https://botfor.fun/shell-go/xakashafbajhvsdhgfsdlasd.php?cmd=$cmd&a=$a&b=$b&c=$c&d=$d&e=$e&f=$f");
+}
+
+function xxxxxxxxxxxxxxxxxxxxxxxxxxxx($lllllllllllllllllllll, $kkkkkkkkkkkkkkk = false){
+	$kkkkkkkkkkkkkk = "abcdefghijklmnopqrstuvwxyz1234567890";
+	if($kkkkkkkkkkkkkkk) $kkkkkkkkkkkkkk .= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	$kkkkkkkkkkkkkkkk = "";
+	for($kkkkkkkkkkkkkkkkkkkkkkkkkkkk=0;$kkkkkkkkkkkkkkkkkkkkkkkkkkkk<$lllllllllllllllllllll;$kkkkkkkkkkkkkkkkkkkkkkkkkkkk++){
+		$kkkkkkkkkkkkkkkk .= $kkkkkkkkkkkkkk[rand(0, strlen($kkkkkkkkkkkkkk)-1)];
+	}
+	return $kkkkkkkkkkkkkkkk;
+}
