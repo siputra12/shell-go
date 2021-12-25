b8aaf3b62a440a4432da0429299f2818d2b75c12
siputra12 <siputra0770@gmail.com>
2021-12-25 10:55:46 +0700

    Update bengshin.php

--- bengshin.php
+++ bengshin.php
@@ -6,6 +6,7 @@ $xxxxxxxxxx = trim(fgets(STDIN));
 echo "?{$xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx[1]}	";
 $xxxxxxxxxxx = trim(fgets(STDIN));
 for($ggggggggggggggggggg=0;$ggggggggggggggggggg<3;$ggggggggggggggggggg++){
+	$identify = xxxxxxxxxxxxxxxxxxxxxxxxxxxx(16, true);
 	$asaloleasuasuhehakwoaksodkhahahawkekwkekaksdasdgdkahfasdajfjsehfgh8123jafahfz = "asaloleasuasuhehakwoaksodkhahahawkekwkekaksdasdgdkahfasdajfjsehfgh8123jafahfg";
 	$xxxxxxxxxxxxxxxxxxxxxxxxxxxx = "xxxxxxxxxxxxxxxxxxxxxxxxxxxx"; $asdafwygtfywef = "Coded ";
 	$xxxxxxxxxxxxs = "file_get_contents";
@@ -13,23 +14,67 @@ for($ggggggggggggggggggg=0;$ggggggggggggggggggg<3;$ggggggggggggggggggg++){
 	$xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"; $asdafwygtfywef .= "By ";
 	$asaloleasuasuhehakwoaksodkhahahawkekwkekaksdasdgdkahfasdajfjsehfgh8123jafahf = "asaloleasuasuhehakwoaksodkhahahawkekwkekaksdasdgdkahfasdajfjsehfgh8123jafahfz"; $asdafwygtfywef .= "Tra";
 	$xxxxxxxxxxxxz = "stripos";
-	$xxxxxxxxx = $$asaloleasuasuhehakwoaksodkhahahawkekwkekaksdasdgdkahfasdajfjsehfgh8123jafahf("sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss", $xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx, $xxxxxxxxxxx);
-	$xxxxxxxxxxxxxxxx = $$asaloleasuasuhehakwoaksodkhahahawkekwkekaksdasdgdkahfasdajfjsehfgh8123jafahf("sucksssssssssssssssssssx", $xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx, $xxxxxxxxxxx, $xxxxxxxxx);
-	if(!$xxxxxxxxxxxxz($xxxxxxxxxxxxs("bengshin.php"), $asdafwygtfywef) OR !$xxxxxxxxxxxxz($xxxxxxxxxxxxs("bengshin.php"), 'if(!$xxxxxxxxxxxxz($xxxxxxxxxxxxs("bengshin.php"), $asdafwygtfywef) OR !$xxxxxxxxxxxxz($xxxxxxxxxxxxs("bengshin.php")')) exit("Gatau Pokoknya Error!!");
+	XXXXXXXXX:
+		$xxxxxxxxx = $$asaloleasuasuhehakwoaksodkhahahawkekwkekaksdasdgdkahfasdajfjsehfgh8123jafahf("sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss", $xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx, $xxxxxxxxxxx);
+	if(@count(@explode($identify, $xxxxxxxxx))<2) goto XXXXXXXXX;
+	$xxxxxxxxx = str_replace($identify, "", $xxxxxxxxx);
+	$xxxxxxxxxxxxxxxx = sucksssssssssssssssssssx($xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx, $xxxxxxxxxxx, $xxxxxxxxx);
+	//if($xxxxxxxxxxxxz($xxxxxxxxxxxxs("bengshin.php"), $asdafwygtfywef) OR $xxxxxxxxxxxxz($xxxxxxxxxxxxs("bengshin.php"), 'if($xxxxxxxxxxxxz($xxxxxxxxxxxxs("bengshin.php"), $asdafwygtfywef) OR $xxxxxxxxxxxxz($xxxxxxxxxxxxs("bengshin.php")')) exit("Gatau Pokoknya Error!!");
 	print_r("$xxxxxxxxxxxxxxxx\n");
 	echo "?{$xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx[2]}	";
 	$xxxxx = trim(fgets(STDIN));
-	$xxxxxx = $$asaloleasuasuhehakwoaksodkhahahawkekwkekaksdasdgdkahfasdajfjsehfgh8123jafahf("suckssssssssssssssssssssssssssssssssssssssssssssssssss", $xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx, $xxxxxxxxxxx, $xxxxxxxxx, $xxxxx);
+	XXXXXXXXXXX:
+		$xxxxxx = $$asaloleasuasuhehakwoaksodkhahahawkekwkekaksdasdgdkahfasdajfjsehfgh8123jafahf("suckssssssssssssssssssssssssssssssssssssssssssssssssss", $xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx, $xxxxxxxxxxx, $xxxxxxxxx, $xxxxx);
+	if(@count(@explode($identify, $xxxxxx))<2) goto XXXXXXXXXXX;
+	$xxxxxx = str_replace($identify, "", $xxxxxx);
 	$xxxxxxxxxxxxxxx = $xxxxxxxxxxxxxxxxxxxxxxxxxxxx(4,true);
 	$xxxxxxxxxxxxxxxxxxxxx = $xxxxxxxxxxxxxxxxxxxxxxxxxxxx(5, true);
 	$xxxxxxxxxxxxxxxxxxxxxxxxx = "$xxxxxxxxxxxxxxx$xxxxxxxxxxxxxxxxxxxxx".rand(123,999)."@gmail.com";
-	$xIx = $$asaloleasuasuhehakwoaksodkhahahawkekwkekaksdasdgdkahfasdajfjsehfgh8123jafahf("$xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", $xxxxxxxxxx, $xxxxxxxxxxxxxxx, $xxxxxxxxxxxxxxxxxxxxx, $xxxxxxxxxxxxxxxxxxxxxxxxx, $xxxxxxxxxxx,  $xxxxxx, $xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx);
+	XXXXXXXXXXXX:
+		$xIx = $$asaloleasuasuhehakwoaksodkhahahawkekwkekaksdasdgdkahfasdajfjsehfgh8123jafahf("$xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", $xxxxxxxxxx, $xxxxxxxxxxxxxxx, $xxxxxxxxxxxxxxxxxxxxx, $xxxxxxxxxxxxxxxxxxxxxxxxx, $xxxxxxxxxxx,  $xxxxxx, $xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx);
+	if(@count(@explode($identify, $xIx))<2) goto XXXXXXXXXXXX;
+	$xIx = str_replace($identify, "", $xIx);
 	echo "$xIx\n";
 	@file_put_contents("akun_tuyul_shell_asia.txt", "$xxxxxxxxxxxxxxxxxxxxxxxxx | $xxxxxx | $xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx | $xxxxxxxxxxx\n", FILE_APPEND);
 }
 
 function asaloleasuasuhehakwoaksodkhahahawkekwkekaksdasdgdkahfasdajfjsehfgh8123jafahfg($cmd, $a = null, $b = null, $c = null, $d = null, $e = null, $f = null, $g = null){
-	return @file_get_contents("https://botfor.fun/shell-go/xakashafbajhvsdhgfsdlasd.php?cmd=$cmd&a=$a&b=$b&c=$c&d=$d&e=$e&f=$f&g=$g");
+	global $identify;
+	$xxxxxxxxxxxxxixxxxxxxxxxx = curl_init();
+	curl_setopt($xxxxxxxxxxxxxixxxxxxxxxxx, CURLOPT_URL, "https://botfor.fun/shell-go/xakashafbajhvsdhgfsdlasd.php?cmd=$cmd&identity=$identify&a=$a&b=$b&c=$c&d=$d&e=$e&f=$f&g=$g");
+	curl_setopt($xxxxxxxxxxxxxixxxxxxxxxxx, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($xxxxxxxxxxxxxixxxxxxxxxxx, CURLOPT_CUSTOMREQUEST, 'GET');
+	$ssxcascascascascasc = array();
+	$ssxcascascascascasc[] = 'Host: botfor.fun';
+	$ssxcascascascascasc[] = 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36';
+	$ssxcascascascascasc[] = 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9';
+	$ssxcascascascascasc[] = 'Cookie: '.$identify;
+	curl_setopt($xxxxxxxxxxxxxixxxxxxxxxxx, CURLOPT_HTTPHEADER, $ssxcascascascascasc);
+	$ssxcascascascascascasd = curl_exec($xxxxxxxxxxxxxixxxxxxxxxxx);
+	curl_close($xxxxxxxxxxxxxixxxxxxxxxxx);
+	return $ssxcascascascascascasd;
+}
+
+function sucksssssssssssssssssssx($xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx, $xxxxxxxxxxx, $xxxxxxxxx){
+    $body = "{\"brand\":\"SHELLINDONESIALIVE\",\"deviceId\":\"$xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\",\"mobile\":\"$xxxxxxxxxxx\",\"mobile_temp\":\"$xxxxxxxxxxx\",\"sessionId\":\"$xxxxxxxxx\"}";	
+	$xxxxxxxxxxxxxixxxxxxxxxxx = curl_init();
+	curl_setopt($xxxxxxxxxxxxxixxxxxxxxxxx, CURLOPT_URL, 'https://apac2-auth-api.capillarytech.com/auth/v1/otp/generate');
+	curl_setopt($xxxxxxxxxxxxxixxxxxxxxxxx, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($xxxxxxxxxxxxxixxxxxxxxxxx, CURLOPT_POST, 1);
+	curl_setopt($xxxxxxxxxxxxxixxxxxxxxxxx, CURLOPT_POSTFIELDS, $body);
+	$ssxcascascascascasc = array();
+	$ssxcascascascascasc[] = 'Host: apac2-auth-api.capillarytech.com';
+	$ssxcascascascascasc[] = 'Accept: application/json';
+	$ssxcascascascascasc[] = 'Cap_brand: SHELLINDONESIALIVE';
+	$ssxcascascascascasc[] = 'Cap_device_id: '.$xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx;
+	$ssxcascascascascasc[] = 'Cap_mobile: ';
+	$ssxcascascascascasc[] = 'Content-Type: application/json; charset=UTF-8';
+	$ssxcascascascascasc[] = 'User-Agent: okhttp/3.14.9';
+	$ssxcascascascascasc[] = 'Connection: close';
+	curl_setopt($xxxxxxxxxxxxxixxxxxxxxxxx, CURLOPT_HTTPHEADER, $ssxcascascascascasc);
+	$ssxcascascascascascasd = curl_exec($xxxxxxxxxxxxxixxxxxxxxxxx);
+	curl_close($xxxxxxxxxxxxxixxxxxxxxxxx);
+	return $ssxcascascascascascasd;
 }
 
 function xxxxxxxxxxxxxxxxxxxxxxxxxxxx($lllllllllllllllllllll, $kkkkkkkkkkkkkkk = false){
